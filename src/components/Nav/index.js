8a8543d7185c472deecff208644b5c8634a5e41d
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-06-10 12:54:01 -0700

    testing gallery

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -32,7 +32,7 @@ function Nav(props) {
                         <span>Contact</span>
                     </li>
                     {categories.map((category) => (
-                        <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
+                        <li className={`mx-1 ${currentCategory.name === category.name}`} key={category.name}>
                             <span onClick={() => {setCurrentCategory(category)}} >
                                 {capitalizeFirstLetter(category.name)}
                             </span>
